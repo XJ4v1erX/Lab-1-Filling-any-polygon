19471797b308fc7e6afb3330f66b5a7c38568082
Javier Alejandro Ramirez Heredia <ram21600@uvg.edu.gt>
2023-11-21 21:59:35 -0600

    Ajustes

--- Polygons.h
+++ Polygons.h
@@ -11,6 +11,24 @@ Color color4(255, 0, 0);    // Rojo
 Color color5(0, 255, 0);    // verde
 Color color6(0, 0, 0); //negro
 
+void drawAndFillPolygon1(Framebuffer& framebuffer) {
+    std::vector<Vertex> vertices1{
+            {165, 380},
+            {185, 360},
+            {180, 330},
+            {207, 345},
+            {233, 330},
+            {230, 360},
+            {250, 380},
+            {220, 385},
+            {205, 410},
+            {193, 383}
+    };
 
+    framebuffer.currentColor = color1;
+    framebuffer.fillPolygon(vertices1);
+    framebuffer.currentColor = color2;
+    framebuffer.drawPolygon(vertices1);
+}
 
 //LAB1_FILLING_ANY_POLYGON_POLYGONS_H
